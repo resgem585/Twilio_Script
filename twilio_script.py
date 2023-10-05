70de46547dd9ff42787e36cf0294c8cb21cbe481
Rodolfo <resgem585@gmail.com>
2023-10-04 21:22:43 -0600

    comment

--- twilio_script.py
+++ twilio_script.py
@@ -1,44 +1,37 @@
-"""
-************************************************************************
-* Author = @alexbonella                                                *
-* Date = '15/09/2022'                                                  *
-* Description = Envio de mensajes Twilio con Python                    *
-************************************************************************
-"""
-
-
 import os
 from twilio.rest import Client
-from twilio_config import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,PHONE_NUMBER,API_KEY_WAPI
+from twilio_config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER, API_KEY_WAPI
 import time
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import pandas as pd
 import requests
-from tqdm import tqdm
+from tqdm import tqdm  # Barra de progreso visual
 from datetime import datetime
-from utils import request_wapi,get_forecast,create_df,send_message,get_date
+from utils import request_wapi, get_forecast, create_df, send_message, get_date  # Importación de funciones personalizadas
 
+# Ciudad y clave de la API del clima
+query = 'cuajimalpa'  # Reemplaza 'cuajimalpa' con la ciudad de tu elección
+api_key = API_KEY_WAPI  # Clave de la API del clima
 
+# Obtiene la fecha actual en el formato deseado
+input_date = get_date()
 
-query = 'cuajimalpa'
-api_key = API_KEY_WAPI
-
-input_date= get_date()
-response = request_wapi(api_key,query)
+# Realiza una solicitud a la API del clima
+response = request_wapi(api_key, query)
 
 datos = []
 
-for i in tqdm(range(24),colour = 'green'):
+# Recopila los datos del pronóstico para las próximas 24 horas
+for i in tqdm(range(24), colour='green'):  # Barra de progreso visual
+    datos.append(get_forecast(response, i))
 
-    datos.append(get_forecast(response,i))
-
-
+# Crea un DataFrame con los datos de pronóstico de lluvia
 df_rain = create_df(datos)
 
-# Send Message
-message_id = send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df_rain,query)
+# Envía un mensaje de Twilio con el pronóstico de lluvia o "Hoy no lloverá"
+message_id = send_message(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, input_date, df_rain, query)
 
-
-print('Mensaje Enviado con exito ' + message_id)
+# Imprime un mensaje de éxito
+print('Mensaje Enviado con éxito ' + message_id)
